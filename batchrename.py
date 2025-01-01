bede19fd20974dd8af6f77e67b035d9df516a130
Evgeny A. Maltsev <yevmal@gmail.com>
2025-01-02 01:12:12 +0300

    Fixed venv

---
+++ batchrename.py
@@ -0,0 +1,12 @@
+program_description: str = "BatchRename by Evgeny A. Maltsev (yevmal@gmail.com)"
+program_version: str = "0.0.1"
+
+
+def batch_rename():
+    return 0
+
+
+if __name__ == '__main__':
+    print(f'{program_description}\nversion {program_version}\n')
+
+    batch_rename()
